5beb2a9165fe3b86b748d0575a95a618adc30ba6
Deflandrenassim <78204743+Deflandrenassim@users.noreply.github.com>
2023-03-14 11:12:06 +0100

    feat : init project

---
+++ my-app/src/pages/index.js
@@ -0,0 +1,10 @@
+import  useMovies  from "@/hook/useMovies"
+
+export default function Home() {
+  return (
+    <>
+      <div className="bg-green-500"> TEST </div>
+      <useMovies />
+    </>
+  )
+}
