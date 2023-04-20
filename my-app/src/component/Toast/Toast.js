03fd2f18d22c116d015cf07b646fe4e2706eb4bd
Deflandrenassim <nassim.def@gmail.com>
2023-04-20 13:40:30 +0200

    no comment

--- my-app/src/component/Toast/Toast.js
+++ my-app/src/component/Toast/Toast.js
@@ -1,29 +1,3 @@
-// import { useState, useContext, createContext, useMemo } from "react"
-
-
-// export const ToastContext = createContext();
-
-// export const ToastProvider = ({ children }) => {
-//     const [active, setActive] = useState(false);
-
-//     // const value = useMemo(() => {
-//     //     active, setActive
-//     // }, [])
-
-//     return (
-//         <ToastContext.Provider value={{ active, setActive }}>
-//             {children}
-//         </ToastContext.Provider>
-//     )
-// }
-
-// export const useToast = () => {
-//     const [active, setActive] = useContext(ToastContext);
-//     console.log("active", active)
-//     const res = { setActive }
-//     return res;
-// }
-
 export const Toast = ({ children, variant }) => {
 
     return (
