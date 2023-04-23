b74a9f1aa77c47a815f7deb44017b17d7273acaf
Deflandrenassim <nassim.def@gmail.com>
2023-04-23 10:03:51 +0200

    fetaure : create context LocalStorageProvider add fonction add localStorage favoris

---
+++ my-app/src/context/LocalStorageProvider.js
@@ -0,0 +1,34 @@
+import { useContext, useMemo, useState, createContext } from "react";
+
+export const MoviesStorageContext = createContext();
+
+export const MoviesStorageProvider = ({ children }) => {
+    const [favoris, setFavoris] = useState([])
+
+    // const value = useMemo(() => ({
+    //     favoris, setFavoris
+    // }), [favoris])
+
+    return (
+        <MoviesStorageContext.Provider value={{ favoris, setFavoris }}>
+            {children}
+        </MoviesStorageContext.Provider>
+    )
+}
+
+export const useMovieFavoris = () => {
+    const { favoris, setFavoris } = useContext(MoviesStorageContext);
+
+    const addLocalStorage = ({ movie }) => {
+        console.log("addLocal", movie)
+        setFavoris([...favoris, movie])
+        let sendLocalStorage = localStorage.setItem("favoris", JSON.stringify(favoris))
+        console.log("stateProvider", favoris);
+        return sendLocalStorage;
+    }
+
+    // const getLocal = () => {
+
+    // }
+    return { addLocalStorage }
+}
--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -1,18 +1,20 @@
 import { ContainerHeader } from "@/container/Header/ContainerHeader";
-import AllMoviesTendance from "@/container/Movie/AllMoviesTendance";
+import { AllMoviesTendance } from "@/container/Movie/AllMoviesTendance";
 import { ContainerFooter } from "@/container/Footer/ContainerFooter";
-// import { ToastProvider } from "@/component/Toast/Toast";
+import { MoviesStorageProvider } from '../context/LocalStorageProvider';
+
 export default function Home() {
 
   return (
     <div>
-      {/* <ToastProvider> */}
-      <ContainerHeader />
-      <div className="p-28">
-        <AllMoviesTendance />
-      </div>
-      <ContainerFooter />
-      {/* </ToastProvider> */}
+      <MoviesStorageProvider>
+        <ContainerHeader />
+        <div className="p-28">
+          <AllMoviesTendance />
+        </div>
+        <ContainerFooter />
+      </MoviesStorageProvider>
+
     </div>
   )
 }
