45e7cf18905fab2722b83346d3758bf55f00f415
Deflandrenassim <nassim.def@gmail.com>
2023-04-26 09:04:18 +0200

    fix : call api async

--- my-app/src/container/Movie/GenreMovie.js
+++ my-app/src/container/Movie/GenreMovie.js
@@ -5,26 +5,25 @@ import { useGetGenreMovie } from "@/hook/useGetGenreMovie"
 
 export const GenreMovie = ({ id }) => {
     const { genreMovie } = useGetGenreMovie()
-    const copyGenreMovie = genreMovie.genres;
+    // const copyGenreMovie = genreMovie.genres;
     console.log("genreMovie", genreMovie);
     let genreName = [];
 
-    useEffect(() => {
-        if (genreMovie) {
-            const filterGenre = () => {
-                for (let i = 0; i < id.length; i++) {
-                    console.log("id", id[i])
-                    let filter = copyGenreMovie.filter(fil => fil.id === id[i])
-                    console.log("filter", filter)
+    // useEffect(() => {
+
+    //     const filterGenre = () => {
+    //         for (let i = 0; i < id.length; i++) {
+    //             console.log("id", id[i])
+    //             let filter = copyGenreMovie.filter(fil => fil.id === id[i])
+    //             console.log("filter", filter)
 
 
-                }
-                console.log("arrName", genreName)
-            }
-            filterGenre();
-        }
-
-    }, [genreMovie])
+    //         }
+    //         console.log("arrName", genreName)
+    //     }
+    //     filterGenre();
+    // }, [])
+    // if (genreMovie) {
 
 
     return (
--- my-app/src/hook/useGetGenreMovie.js
+++ my-app/src/hook/useGetGenreMovie.js
@@ -1,14 +1,21 @@
 import { useState, useEffect } from "react"
 import axios from "axios"
 
-export const useGetGenreMovie = () => {
+export const useGetGenreMovie = async () => {
     const [genreMovie, setGenreMovie] = useState([])
+
     useEffect(() => {
-        axios.get(`https://api.themoviedb.org/3//genre/movie/list?api_key=0670a5a788c4054210c0beadffe00f82`)
-            .then((res) => {
-                setGenreMovie(res.data)
-            })
+        const fetchData = async () => {
+            const data = await axios.get(`https://api.themoviedb.org/3//genre/movie/list?api_key=0670a5a788c4054210c0beadffe00f82`)
+                .then((res) => {
+                    setGenreMovie(res.data)
+                })
+            return data
+        }
+        fetchData()
     }, [])
+
+
     return { genreMovie }
 
 }
