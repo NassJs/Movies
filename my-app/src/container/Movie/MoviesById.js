ae7f1a5590636f8fc4d8fce786115f752416ee19
Deflandrenassim <nassim.def@gmail.com>
2023-05-01 10:29:57 +0200

    feature : display actors

--- my-app/src/container/Movie/MoviesById.js
+++ my-app/src/container/Movie/MoviesById.js
@@ -21,20 +21,22 @@ export const MoviesById = () => {
                 <div className='m-2 mt-28 p-10'>
                     <Button className="h-12 w-20 border-2 bg-slate-100 hover:bg-slate-400 rounded-full" onClick={() => router.back()}>  Retour </Button>
                     {moviesId.map((movie) => (
-                        <div key={movie.id} className='flex p-4'>
-                            <Picture src={`https://image.tmdb.org/t/p/original//${movie.poster_path}`}
-                                size="h-104 w-72 rounded-lg shadow-2xl" />
-                            <div className="p-4">
-                                <Title variant="text-3xl"> {movie.title} </Title>
-                                <Score> {movie.vote_average} / 10 </Score>
-                                <Note> (Vote user : {movie.vote_count})</Note>
-                                <GenreMovie id={movie.genre_ids} />
-                                <SubTitle variant="text-xl pt-4 pb-4"> {movie.overview}</SubTitle>
-                                <Button> <BiLike /> </Button>
-                                <Button> <BiDislike /> </Button>
+                        <>
+                            <div key={movie.id} className='flex p-4'>
+                                <Picture src={`https://image.tmdb.org/t/p/original//${movie.poster_path}`}
+                                    size="h-104 w-72 rounded-lg shadow-2xl" />
+                                <div className="p-4">
+                                    <Title variant="text-3xl"> {movie.title} </Title>
+                                    <Score> {movie.vote_average} / 10 </Score>
+                                    <Note> (Vote user : {movie.vote_count})</Note>
+                                    <GenreMovie id={movie.genre_ids} />
+                                    <SubTitle variant="text-xl pt-4 pb-4"> {movie.overview}</SubTitle>
+                                    <Button> <BiLike /> </Button>
+                                    <Button> <BiDislike /> </Button>
+                                </div>
                             </div>
                             <ActorById id={movie.id} />
-                        </div>
+                        </>
                     ))}
                 </div>
             </>
--- my-app/src/hook/useActor.js
+++ my-app/src/hook/useActor.js
@@ -4,16 +4,17 @@ import { useEffect, useState } from "react";
 export const useActor = ({ id }) => {
 
     const [actors, setActors] = useState([]);
-    console.log("state actor", actors)
     useEffect(() => {
-
         const fetchData = async () => {
             await axios.get(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=0670a5a788c4054210c0beadffe00f82&language=en-US`)
                 .then((res) => {
-                    setActors(res.data.cast)
+                    // for (let i = 0; i < res.data.cast.length; i++) {
+                    setActors(...actors, res.data.cast)
+                    // }
+                    // setActors(res.data.cast)
                 })
         }
         fetchData();
     }, [])
-    return { actors }
+    return actors
 }
